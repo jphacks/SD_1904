6c2cfbeb57051825b33610113b2e16cf30652b7c
daikihashimoto <hashimotodaiki@hashimotodaikinoMacBook-puro.local>
2019-10-26 15:14:55 +0900

    seconds commit

--- zekki/components/Home.js
+++ zekki/components/Home.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+<<<<<<< Updated upstream
 import {View, Text, Button} from 'react-native';
 import {Actions} from 'react-native-router-flux';
+=======
+
+import AlarmInfo from './AlarmInfo';
+>>>>>>> Stashed changes
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      alarms: [
+        {time: 2345, days: [false, false, true, true, true, true, false]},
+        {time: 1233, days: [false, false, true, true, true, true, false]},
+      ],
+    };
+    console.log(this.state.alarms);
   }
 
   render() {
@@ -13,6 +25,11 @@ export default class Home extends Component {
         <Button title="add" onPress={() => Actions.addAlarm()} />
         <Button title="setting" onPress={() => Actions.setting()} />
         <Text>Home</Text>
+        {this.state.alarms.map(e => {
+          return <AlarmInfo times={e.time} days={e.days} />;
+        })}
+        {/* <AlarmInfo times={2345} days={['asd', 'asds']} />
+        <AlarmInfo times={2345} days={['asd', 'asds']} /> */}
       </View>
     );
   }
