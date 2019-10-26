08eff737359ffdc49146e72b3f3239303ea0e042
matatabinoneko <matatabinoneko0721@gmail.com>
2019-10-26 15:01:45 +0900

    music

--- zekki/components/Alarm.js
+++ zekki/components/Alarm.js
@@ -1,10 +1,63 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {
+  Button,
+  View,
+  Text,
+} from 'react-native';
+
+import TrackPlayer from 'react-native-track-player';
+
+import TrackPlayerEventTypes from 'react-native-track-player';
+
+var music = {
+  id: 'unique track id', // Must be a string, required
+  
+  url: require('./trumpet1.mp3'), // Load media from the network
+
+};
+
+
+TrackPlayer.setupPlayer().then(() => {
+  TrackPlayer.addEventListener('playback-queue-ended', (track, position) => {
+    TrackPlayer.add([music])
+  });
+  // The player is ready to be used
+});
+
+
+TrackPlayer.add([music]).then(function() {
+  // The tracks were added
+});
+
+
 
 export default class Alarm extends Component {
   render() {
     return (
       <View>
+        <Button
+          title="Start button"
+          onPress={() => {
+            TrackPlayer.setVolume(1)
+            TrackPlayer.play()
+          }}
+        />
+        <Button
+          title="Stop button"
+          onPress={() => {
+            TrackPlayer.stop()
+          }}
+        />   
+        <Button
+          title="State button"
+          onPress={() => {TrackPlayer.getState().then(state => {
+            console.log(state)
+            // console.log("event", playback-queue-ended)
+          });
+          }}
+        />  
+        
+
         <Text>Alarm</Text>
       </View>
     );
