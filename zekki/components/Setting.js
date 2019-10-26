79ecb219c1b118df4198a03a9ebdcd8f7d7b8841
Kaito Suzuki <kaito@KaitonoMacBook-Pro.local>
2019-10-26 14:39:45 +0900

    add code to pick audio from directory using react-native-documetn-picker

--- zekki/components/Setting.js
+++ zekki/components/Setting.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {View, Button} from 'react-native';
 import {Actions} from 'react-native-router-flux';
+import DocumentPicker from 'react-native-document-picker';
 
 export default class Setting extends Component {
   render() {
@@ -8,8 +9,16 @@ export default class Setting extends Component {
       <View>
         <Button title="NFC" onPress={() => Actions.nfc()} />
         <Button title="デフォルト設定" onPress={() => Actions.addAlarm()} />
+        <Button title="アラーム音の追加" onPress={() => this.pickaudio()} />
         <Button title="ライセンス" />
       </View>
     );
+  }
+
+  async pickaudio() {
+    const res = await DocumentPicker.pick({
+      type: [DocumentPicker.types.audio],
+    });
+    console.log(res);
   }
 }
