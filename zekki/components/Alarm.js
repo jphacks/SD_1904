1e8d87f16331ba97185a1fe793f6cecbceac5298
Kaito Suzuki <kaito@KaitonoMacBook-Pro.local>
2019-10-27 11:16:39 +0900

    integrate alarm start and stop

--- zekki/components/Alarm.js
+++ zekki/components/Alarm.js
@@ -3,6 +3,7 @@ import {Button, View, Text} from 'react-native';
 
 import TrackPlayer from 'react-native-track-player';
 import SystemSetting from 'react-native-system-setting'
+import NfcManager, {NfcEvents} from 'react-native-nfc-manager';
 
 import TrackPlayerEventTypes from 'react-native-track-player';
 
@@ -11,23 +12,39 @@ var music = {
   url: require('./trumpet1.mp3'), // Load media from the network
 };
 
-TrackPlayer.setupPlayer().then(() => {
-  TrackPlayer.add([music]).then(function() {
-    // SystemSetting.setVolume(1.0);
-    TrackPlayer.play();
-  });
-
-  const volumeListener = SystemSetting.addVolumeListener((data) => {
-    // SystemSetting.setVolume(1.0);
-  });
-
-  TrackPlayer.addEventListener('playback-queue-ended', (track, position) => {
-    TrackPlayer.add([music]);
-  });
-  // The player is ready to be used
-});
-
 export default class Alarm extends Component {
+  constructor(props){
+    super(props);
+    TrackPlayer.setupPlayer().then(() => {
+      TrackPlayer.add([music]).then(function() {
+        // SystemSetting.setVolume(1.0);
+        TrackPlayer.play();
+      });
+    
+      const volumeListener = SystemSetting.addVolumeListener((data) => {
+        // SystemSetting.setVolume(1.0);
+      });
+    
+      TrackPlayer.addEventListener('playback-queue-ended', (track, position) => {
+        TrackPlayer.add([music]);
+      });
+      // The player is ready to be used
+    });
+
+    (async () => {
+      try {
+        NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
+          NfcManager.unregisterTagEvent().catch(() => 0);
+          console.warn('NFC read');
+          TrackPlayer.stop();
+        });
+        await NfcManager.registerTagEvent();
+      } catch (ex) {
+          console.warn('ex', ex);
+          NfcManager.unregisterTagEvent();
+    }})();
+
+  }
   render() {
     return (
       <View>
